8726a426b51b6cb86f6a11d1223b657162c461df
jhonnyzp <jhonnyzp@outlook.com>
2022-09-01 02:34:27 -0400

    actualizar query and controller

--- backend/src/controller/dispositivo.controller.js
+++ backend/src/controller/dispositivo.controller.js
@@ -1,11 +1,11 @@
 const dbConnection = require('../config/databaseCon');
 const connection = dbConnection();
 let getDispositivo = async (req,res)=>{
-    await connection.query("select * from Dispositivo", (err,result)=>{
+    await connection.query("select * from Dispositivo;", (err,result)=>{
         if (result)    
             res.send(result);
         else
-            res.status(500).send(err);s
+            res.status(500).send(err);
     });
 }
 
--- backend/src/controller/ubicacion.controller.js
+++ backend/src/controller/ubicacion.controller.js
@@ -1,11 +1,11 @@
 const dbConnection = require('../config/databaseCon');
 const connection = dbConnection();
 let getUbicacion = async (req,res)=>{
-    await connection.query("select * from Ubicacion", (err,result)=>{
+    await connection.query("select * from Ubicacion;", (err,result)=>{
         if (result)    
             res.send(result);
         else
-            res.status(500).send(err);s
+            res.status(500).send(err);
     });
 }
 
